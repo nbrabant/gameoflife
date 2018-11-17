95564e6b8211de906aea34ceb5cab6ffc795f977
nbrabant <brabantnicolas59@gmail.com>
2018-11-17 16:40:14 +0100

    should be alive if it alive and there are 2 neighbors

--- gameoflife.spec.js
+++ gameoflife.spec.js
@@ -1,4 +1,7 @@
-const isAlive = function (number) {
+const isAlive = function (number, alive) {
+    if(alive == true && number == 2){
+    return true;
+        }
     return number === 3;
 }
 
@@ -13,6 +16,8 @@ describe('game of life', () => {
     it('should be alive if there are 3 neighbors', () => {
         expect(isAlive(3)).toBeTruthy()
     })
-
+    it('should be alive if it alive and there are 2 neighbors', () => {
+        expect(isAlive(2, true)).toBeTruthy()
+    })
 
 })
