b0db2016308e933ad07ca297453934b8dac25cc8
nbrabant <brabantnicolas59@gmail.com>
2019-11-16 13:33:22 +0100

    feat(cell) : add basic tests for alive and dead cell

---
+++ gameoflife.spec.js
@@ -0,0 +1,15 @@
+const isAlive = (currentState, numberOfneightboors) => {
+    return true
+};
+
+describe('game of life', () => {
+    describe('game of life -- alive or dead', () => {
+        it('should dead cell be alive if has 3 neigthboors', () => {
+            expect(isAlive(false, 3)).toBeTruthy()
+        })
+
+        it('should alive cell be dead if has 1 neigthboor', () => {
+            expect(isAlive(true, 3)).toBeFalsy()
+        })
+    })
+})
